96776dd4e37d9b91cdadaff32164aea66990e692
Bharath1998 <bharath.raghunath@gmail.com>
2018-04-15 12:34:46 +0530

    hey

---
+++ p.py
@@ -0,0 +1,208 @@
+# Dynamic Programming Python implementation of Coin
+# Change problem
+def count(S, m, n):
+    # We need n+1 rows as the table is constructed
+    # in bottom up manner using the base case 0 value
+    # case (n = 0)
+    table = [[0 for x in range(m)] for x in range(n+1)]
+
+    # Fill the entries for 0 value case (n = 0)
+    for i in range(m):
+        table[0][i] = 1
+
+    # Fill rest of the table entries in bottom up manner
+    for i in range(1, n+1):
+        for j in range(m):
+
+            # Count of solutions including S[j]
+            x = table[i - S[j]][j] if i-S[j] >= 0 else 0
+
+            # Count of solutions excluding S[j]
+            y = table[i][j-1] if j >= 1 else 0
+
+            # total count
+            table[i][j] = x + y
+
+    return table[n][m-1]
+
+# Driver program to test above function
+arr = [1, 2, 3]
+m = len(arr)
+n = 4
+print(count(arr, m, n))
+
+# This code is contributed by Bhavya Jain
+
+
+
+
+
+
+
+
+
+
+def lcs(X , Y):
+    # find the length of the strings
+    m = len(X)
+    n = len(Y)
+
+    # declaring the array for storing the dp values
+    L = [[None]*(n+1) for i in xrange(m+1)]
+
+    """Following steps build L[m+1][n+1] in bottom up fashion
+    Note: L[i][j] contains length of LCS of X[0..i-1]
+    and Y[0..j-1]"""
+    for i in range(m+1):
+        for j in range(n+1):
+            if i == 0 or j == 0 :
+                L[i][j] = 0
+            elif X[i-1] == Y[j-1]:
+                L[i][j] = L[i-1][j-1]+1
+            else:
+                L[i][j] = max(L[i-1][j] , L[i][j-1])
+
+    # L[m][n] contains the length of LCS of X[0..n-1] & Y[0..m-1]
+    return L[m][n]
+#end of function lcs
+
+
+# Driver program to test the above function
+X = "AGGTAB"
+Y = "GXTXAYB"
+print "Length of LCS is ", lcs(X, Y)
+
+# This code is contributed by Nikhil Kumar Singh(nickzuck_007)
+/ A Dynamic Programming based C++ program for LPS problem
+// Returns the length of the longest palindromic subsequence in seq
+#include<stdio.h>
+#include<string.h>
+
+// A utility function to get max of two integers
+int max (int x, int y) { return (x > y)? x : y; }
+
+// Returns the length of the longest palindromic subsequence in seq
+int lps(char *str)
+{
+   int n = strlen(str);
+   int i, j, cl;
+   int L[n][n];  // Create a table to store results of subproblems
+
+
+   // Strings of length 1 are palindrome of lentgh 1
+   for (i = 0; i < n; i++)
+      L[i][i] = 1;
+
+    // Build the table. Note that the lower diagonal values of table are
+    // useless and not filled in the process. The values are filled in a
+    // manner similar to Matrix Chain Multiplication DP solution (See
+    // https://www.geeksforgeeks.org/archives/15553). cl is length of
+    // substring
+    for (cl=2; cl<=n; cl++)
+    {
+        for (i=0; i<n-cl+1; i++)
+        {
+            j = i+cl-1;
+            if (str[i] == str[j] && cl == 2)
+               L[i][j] = 2;
+            else if (str[i] == str[j])
+               L[i][j] = L[i+1][j-1] + 2;
+            else
+               L[i][j] = max(L[i][j-1], L[i+1][j]);
+        }
+    }
+
+    return L[0][n-1];
+}
+
+/* Driver program to test above functions */
+int main()
+{
+    char seq[] = "GEEKS FOR GEEKS";
+    int n = strlen(seq);
+    printf ("The lnegth of the LPS is %d", lps(seq));
+    getchar();
+    return 0;
+}
+
+
+
+
+
+
+# A Python program for Prim's Minimum Spanning Tree (MST) algorithm.
+# The program is for adjacency matrix representation of the graph
+
+import sys  # Library for INT_MAX
+
+class Graph():
+
+    def __init__(self, vertices):
+        self.V = vertices
+        self.graph = [[0 for column in range(vertices)]
+                      for row in range(vertices)]
+
+    # A utility function to print the constructed MST stored in parent[]
+    def printMST(self, parent):
+        print "Edge \tWeight"
+        for i in range(1,self.V):
+            print parent[i],"-",i,"\t",self.graph[i][ parent[i] ]
+
+    # A utility function to find the vertex with minimum distance value, from
+    # the set of vertices not yet included in shortest path tree
+    def minKey(self, key, mstSet):
+
+        # Initilaize min value
+        min = sys.maxint
+
+        for v in range(self.V):
+            if key[v] < min and mstSet[v] == False:
+                min = key[v]
+                min_index = v
+
+        return min_index
+
+    # Function to construct and print MST for a graph represented using
+    # adjacency matrix representation
+    def primMST(self):
+
+        #Key values used to pick minimum weight edge in cut
+        key = [sys.maxint] * self.V
+        parent = [None] * self.V # Array to store constructed MST
+        key[0] = 0   # Make key 0 so that this vertex is picked as first vertex
+        mstSet = [False] * self.V
+
+        parent[0] = -1  # First node is always the root of
+
+        for cout in range(self.V):
+
+            # Pick the minimum distance vertex from the set of vertices not
+            # yet processed. u is always equal to src in first iteration
+            u = self.minKey(key, mstSet)
+
+            # Put the minimum distance vertex in the shortest path tree
+            mstSet[u] = True
+
+            # Update dist value of the adjacent vertices of the picked vertex
+            # only if the current distance is greater than new distance and
+            # the vertex in not in the shotest path tree
+            for v in range(self.V):
+                # graph[u][v] is non zero only for adjacent vertices of m
+                # mstSet[v] is false for vertices not yet included in MST
+                # Update the key only if graph[u][v] is smaller than key[v]
+                if self.graph[u][v] > 0 and mstSet[v] == False and
+                   key[v] > self.graph[u][v]:
+                        key[v] = self.graph[u][v]
+                        parent[v] = u
+
+        self.printMST(parent)
+
+g  = Graph(5)
+g.graph = [ [0, 2, 0, 6, 0],
+             [2, 0, 3, 8, 5],
+             [0, 3, 0, 0, 7],
+             [6, 8, 0, 0, 9],
+             [0, 5, 7, 9, 0],
+           ]
+
+g.primMST();
